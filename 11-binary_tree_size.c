4b6ffe5dea7d4e13679cb7602a08bbc7957bcbc2
Felipe Santos <felipecassiors@gmail.com>
2024-01-11 17:25:38 -0500

    11-size

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -7,9 +7,9 @@
  * Return: The size of the tree. If tree is NULL, return 0.
  */
 size_t binary_tree_size(const binary_tree_t *tree) {
-    if (tree == NULL) {
-        return 0;
-    }
+	if (tree == NULL) {
+		return 0;
+	}
 
-    return 1 + binary_tree_size(tree->left) + binary_tree_size(tree->right);
+	return 1 + binary_tree_size(tree->left) + binary_tree_size(tree->right);
 }
