49c380747816c5f5a808dd108465080d896deb0f
Juan C Lopez <juancalpz@yahoo.com>
2024-01-11 09:53:33 -0400

    Betty fix

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,9 +7,10 @@
  * @value: value to put in a new node
  * Return: A pointer to the new node, or NULL on failure
  */
-binary_tree_t *binary_tree_node(binary_tree_t * parent, int value)
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+
 	if (new_node == NULL)
 	{
 		return (NULL);
