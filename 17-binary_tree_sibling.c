73d3295d390d2853d2b684bdbe65589fccdba6dd
Juan C Lopez <juancalpz@yahoo.com>
2024-01-12 01:01:12 -0400

    Betty fix

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -3,19 +3,22 @@
 /**
  * binary_tree_sibling - Finds the sibling of a node
  * @node: Pointer to the node to find the sibling
- *
  * Return: Pointer to the sibling node, NULL if no sibling or node is NULL
  */
-binary_tree_t *binary_tree_sibling(binary_tree_t *node) {
-    if (node == NULL || node->parent == NULL) {
-        return NULL;
-    }
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+	{
+		return (NULL);
+	}
+	binary_tree_t *parent = node->parent;
 
-    binary_tree_t *parent = node->parent;
-
-    if (parent->left == node) {
-        return parent->right;
-    } else {
-        return parent->left;
-    }
+	if (parent->left == node)
+	{
+		return (parent->right);
+	}
+	else
+	{
+		return (parent->left);
+	}
 }
